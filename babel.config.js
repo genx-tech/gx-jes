04c51cbd3900312bd5ec8393279d36e8cffd7c22
Rockie Guo <rockie@kitmi.com.au>
2021-06-10 15:45:12 +1000

    fix: built files

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,31 @@
+const targetLTSVersion = '12.0';
+
 module.exports = function (api) {
     let isProduction = api.env(['production']);
 
-    return {        
+    return {
+        env: {
+            test: {
+                presets: [
+                    [
+                        '@babel/env',
+                        {
+                            useBuiltIns: 'usage',
+                            corejs: { version: '3.8', proposals: true },
+                        },
+                    ],
+                ],
+            },
+        },
         presets: [
             [
                 '@babel/env',
+                {
+                    targets: {
+                        node: targetLTSVersion,
+                    },
+                    exclude: ['@babel/plugin-transform-regenerator'],
+                },
             ],
         ],
         plugins: [
